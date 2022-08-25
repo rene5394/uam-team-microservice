e57aaa298c5d4638e35aceaae3a513be9250fd0c
reneuam <renet@uassistme.com>
2022-08-24 21:32:24 -0600

    Add user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './api/user/user.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       autoLoadEntities: true,
       synchronize: false
     }),
+    UserModule,
   ]
 })
 export class AppModule {}
