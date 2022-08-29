71d6cb2b8e43d597d5616674c79dd6646267d05f
reneuam <renet@uassistme.com>
2022-08-29 16:15:02 -0600

    Add findAll function

--- src/api/user/user.service.ts
+++ src/api/user/user.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { UserStatus } from 'src/common/constants';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
 
@@ -10,7 +11,29 @@ export class UserService {
     private readonly userRepository: Repository<User>
   ) {}
 
+  async findAll(status: string) {
+    const keys = Object.keys(UserStatus);
+    let statusId = 0;
+
+    Object.keys(UserStatus).forEach(key => {
+      if (key === status) {
+        statusId = UserStatus[key];        
+      }
+    });
+
+    if (statusId) {
+      return await this.userRepository.find({ where : { status_id : statusId} });
+    }
+
+    return await this.userRepository.find();
+  }
+
   async findOne(id: number) {
-    return await this.userRepository.findOne({ where: { id: id } });
+    return await this.userRepository.findOne({
+        relations: {
+            images: true,
+        },
+        where: { id }
+    });
   }
 }
