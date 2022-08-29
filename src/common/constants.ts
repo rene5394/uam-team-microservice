c7e0158741f982f21d806bdd22985b847244d961
reneuam <renet@uassistme.com>
2022-08-29 16:19:39 -0600

    Add team queue, userstatuses and team statuses

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,3 +1,14 @@
 export enum RabbitMQ {
-    UserQueue = 'teamUser'
+  UserQueue = 'teamUser',
+  TeamQueue = 'teamTeam'
+}
+
+export const UserStatus = {
+  active : 1,
+  inactive : 3
+}
+
+export const TeamStatus = {
+  active : 0,
+  inactive : 1
 }
