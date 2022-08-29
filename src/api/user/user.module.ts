70626517c6ebd7ba8e8f42af403f73d0780a255b
reneuam <renet@uassistme.com>
2022-08-29 16:14:06 -0600

    Add Image typeORM entity

--- src/api/user/user.module.ts
+++ src/api/user/user.module.ts
@@ -3,9 +3,10 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { User } from './entities/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Image } from './entities/image.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, Image])],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService]
